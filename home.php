4225534cb0a79ac28c46b592451675470e070ea5
vinjerdim73 <marvinbudiman73@guru.smk.belajar.id>
2024-01-13 06:51:07 +0700

    tambah array_key_exists

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if (!$_SESSION["username"]) {
+    if (!array_key_exists("username", $_SESSION)) {
         // jika di sesi ini tidak ada username yang aktif
         // proses ke logout
         header("location:logout.php");
